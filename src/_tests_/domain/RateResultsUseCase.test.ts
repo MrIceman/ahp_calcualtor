baac98a4a6052f60e030a8f4308ebaa76a3a1b63
mrtn <mrtnnwsd@gmail.com>
2018-05-31 17:10:11 +0200

    MatrixCalculator + Tests and UseCases

--- src/_tests_/domain/RateResultsUseCase.test.ts
+++ src/_tests_/domain/RateResultsUseCase.test.ts
@@ -1,4 +1,4 @@
-import {RateResultsUseCaseImpl} from "../../domain/impl/RateResultsUseCaseImpl";
+import {NominatedValue, RateResultsUseCaseImpl} from "../../domain/impl/RateResultsUseCaseImpl";
 import {Repository} from "../../data/Repository";
 import {Criteria} from "../../data/model/Criteria";
 import {Goal} from "../../data/model/Goal";
@@ -9,7 +9,7 @@ const competition = new Criteria('competition');
 const custFreq = new Criteria('cust freq');
 const rent = new Criteria('rent');
 visiblity.comparisionValues.set(competition.name, 5);
-visiblity.comparisionValues.set(rent.name, 1/3);
+visiblity.comparisionValues.set(rent.name, 1 / 3);
 visiblity.comparisionValues.set(custFreq.name, 1);
 
 competition.comparisionValues.set(custFreq.name, 1);
@@ -33,6 +33,22 @@ repository.insertCriteria(rent);
 const subject = new RateResultsUseCaseImpl(repository);
 
 it('calculates correct global normed values', () => {
+    const expectedResult: Array<NominatedValue> = [
+        {
+            "key": 'visiblity', "value": 0.23855311355311354
+        },
+        {
+            "key": "competition", "value": 0.0989010989010989
+        },
+        {
+            "key": "cust freq", "value": 0.15521978021978022
+        },
+        {
+            "key": "rent", "value": 0.5073260073260073
+        }
+    ];
     const result = subject.getCriteriaPriority(subject.getRatedCriteria());
-    expect(result).toEqual(undefined);
+    expect(result).toEqual(expectedResult);
+
+
 });
